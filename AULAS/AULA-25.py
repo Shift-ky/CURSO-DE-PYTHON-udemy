e8482bddf00b2621fb99cf630586a654167678b1
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-04-25 12:33:58 -0300

    Commmit do final da aula

--- AULAS/AULA-25.py
+++ AULAS/AULA-25.py
@@ -7,7 +7,14 @@ Interpolação básica de strings
 
 nome = 'Robson'
 valor = 1.200
-
+variavel = 'AB'
 
 print('Olá %s o valor da compra foi %.2f '%(nome,valor))
 
+''' formatações que dá para fazer em f string '''
+
+print(f'{variavel:->10}.')
+print(f'{variavel:-<10}.')
+print(f'{variavel:-^10}.')
+print(f'O Hexadecimal de 1500 é {1500:8X}')
+ 
