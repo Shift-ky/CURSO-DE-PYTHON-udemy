6f8a5db92490392b687fb975a4ca6df8794c4d9c
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-04-25 15:14:22 -0300

    finalização da atividade

--- ATIVIDADES/EXERCICIO - AULA 55-2.py
+++ ATIVIDADES/EXERCICIO - AULA 55-2.py
@@ -3,3 +3,13 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+print("\033c", end="")
+
+nome = input('Digite o seu nome: ')
+
+if len(nome) <=4:
+    print('Seu nome é curto')
+elif len(nome) >=7:
+    print('Seu nome é grande')
+else:
+    print('Seu nome é normal')
