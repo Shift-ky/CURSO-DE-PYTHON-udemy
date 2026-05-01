b76af2a4b59c8c3b8c65437554ebd3db5e52635b
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-01 14:31:49 -0300

    Finalização da atividade que valida um CPF

--- ATIVIDADES/EXERCÍCIO - AULA 99-final.py
+++ ATIVIDADES/EXERCÍCIO - AULA 99-final.py
@@ -34,7 +34,6 @@ if len(cpf) != 11:
 
 try:
     
-    #Variáveis do primeiro dígito
     cpf_nove_digitos = cpf[0:9] 
     multiplicador_nove_digitos = int(10)
     multiplicador_nove_digitos_soma = 0
@@ -53,14 +52,31 @@ try:
     resto_divissão_nove_digitos = nove_digitos_multiplicado_por_dez % 11
     digito_validador_1 = resto_divissão_nove_digitos if resto_divissão_nove_digitos < 9 else 0 
 
+    
+    
+    multiplicador_dez_digitos = 11
+    dez_digito_multiplicado = 0
+    dez_digito_mutiplicado_dez = 0 
 
-    if cpf[9] == str(digito_validador_1):
+    cpf_dez_digitos = cpf[:9] + str(digito_validador_1)
+
+    for j in cpf_dez_digitos:
+        dez_digito_multiplicado += int(j) * multiplicador_dez_digitos
+        multiplicador_dez_digitos -= 1
+    
+    dez_digito_mutiplicado_dez = dez_digito_multiplicado * 10
+
+    resto_divisao_dez_digito = dez_digito_mutiplicado_dez % 11
+    digito_validador_2 = resto_divisao_dez_digito if resto_divisao_dez_digito < 9 else 0
+
+
+    if cpf[9] == str(digito_validador_1) and cpf[10] == str(digito_validador_2):
         print('CPF VÁLIDO') 
     else:
         print('CPF INVÁLIDO')
 except IndexError:
     print('CPF Invormado está faltando informações, informar um CPF válido')
-#except TypeError:
- #   print('CPF com quantidade incorreta de caracteres')
+except TypeError:
+    print('CPF com quantidade incorreta de caracteres')
 except ValueError:
     print('CPF com letras informado, por gentileza inform os 11 dígitos do seu CPF')
