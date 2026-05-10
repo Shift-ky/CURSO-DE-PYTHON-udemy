5e9a9bc3749bc4b9109281b3e5f6201e5b8bd664
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-10 12:56:20 -0300

    onclusão da atividade

--- ATIVIDADES/EXERCICIO - AULA -  133.py
+++ ATIVIDADES/EXERCICIO - AULA -  133.py
@@ -11,22 +11,33 @@ Requisitos:
         [1, 4, 9, 8, ->9<-, 4, 8] (retorne 9)
     Se não encontrar duplicados na lista, retorne -1
 """
- 
-lista = [1, 2, 3, 4, 5, 6]
-numeros = set()
-primeiro_duplicado = None
-duplicado = False
-
-for i in lista:
-    duplicado = i in numeros
-    if duplicado and primeiro_duplicado is None:
-        primeiro_duplicado = i
-    else:
-        primeiro_duplicado = -1
-    numeros.add(i)
+def numero_duplicado(lista):
+    numeros = set()
+    for i in lista:
+        if i in numeros:
+            return i
+        numeros.add(i)
+        
+        
+    return -1
+   
     
-    
+lista_de_listas_de_inteiros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
 
-print(f'primeiro duplicado é {primeiro_duplicado}')
-    
 
+
+for listas in lista_de_listas_de_inteiros:
+    print(f'Na lista {listas}, O primeiro valor duplicado é {numero_duplicado(listas)}')
